cd8be8b9a159bd5163b43c711c49aa6cd0d25946
Federico Carrasco <federicogcarrasco@gmail.com>
2026-04-12 19:23:54 -0300

    Fix error: Bad credentials exception not being captured

--- src/main/java/com/challenge/shoppingcart/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/challenge/shoppingcart/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.challenge.shoppingcart.exceptions;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.authentication.BadCredentialsException;
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
@@ -12,6 +14,12 @@ import java.util.stream.Collectors;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentials(BadCredentialsException ex) {
+        log.warn("Bad credentials: {}", ex.getMessage());
+        return buildError(HttpStatus.UNAUTHORIZED, "Usuario o contraseña incorrectos");
+    }
 
     @ExceptionHandler(UnauthorizedOperationException.class)
     public ResponseEntity<ApiError> handleUnauthorized(UnauthorizedOperationException ex) {
